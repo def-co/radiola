56790948f625902901daa4480610e166320b6b83
paulsnar <paulsnar@paulsnar.lv>
2019-07-03 23:29:55 +0300

    Add graceful shutdown for server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 "use strict";
 
-const http = require('http'),
+const fs = require('fs'),
+      http = require('http'),
       { handler } = require('./lib/server');
 
 const server = http.createServer(handler);
 server.listen('/run/p22-radiola/http-api.sock');
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    fs.unlinkSync('/run/p22-radiola/http-api.sock');
+  });
+});
