f067b9e1ae633d216b68c1c51e43a439f5e0b171
paulsnar <paulsnar@paulsnar.lv>
2020-02-17 01:32:00 +0200

    Attempt to implement more graceful shutdown

--- server.js
+++ server.js
@@ -4,9 +4,27 @@ const fs = require('fs'),
       http = require('http'),
       { handler } = require('./lib/server');
 
+let connections = [ ];
+
 const server = http.createServer(handler);
+server.on('connection', (socket) => {
+  connections.push(socket);
+  socket.on('close', () => {
+    let index = connections.indexOf(socket);
+    if (index !== -1) {
+      connections.splice(index, 1);
+    }
+  });
+});
 server.listen('/run/p22-radiola/http-api.sock');
 
 process.on('SIGTERM', () => {
-  server.close();
+  let quitTimeout = setTimeout(() => {
+    connections.forEach((socket) => {
+      socket.end();
+    });
+  }, 1000);
+  server.close(() => {
+    clearTimeout(quitTimeout);
+  });
 });
