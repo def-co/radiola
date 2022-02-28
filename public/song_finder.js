04cf45666d9b809877d40a657a652d89666cbadf
paulsnar <paulsnar@paulsnar.lv>
2022-02-28 18:16:04 +0200

    Finder: +kurzemes +viegli -stalica

--- public/song_finder.js
+++ public/song_finder.js
@@ -1,7 +1,7 @@
 /*
  * P22 Radiola
  *
- * @version 1.2.0 (Akords)
+ * @version 1.2.1 (Akords)
  * @author paulsnar <paulsnar@paulsnar.lv>
  * @license © 2016-2018 paulsnar. All Rights Reserved.
  */
@@ -13,7 +13,7 @@
     star_fm: true,
     lr1: true, lr2: true, lr3: true, lr4: true,
     lr_naba: true,
-    stalica: true,
+    viegli: true, kurzemes: true,
     ehr: true, ehr_superhits: true, ehr_kh: true, ehr_summer: true,
       ehr_eurotop: true, ehr_fresh: true, ehr_latv_hiti: true,
       ehr_top_40: true, ehr_darbam: true, ehr_ritam: true, ehr_fitness: true,
